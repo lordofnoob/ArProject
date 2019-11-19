4bb54a67bc42cc8910de50d17526e4b3fb00fb7e
PIAU Timothée <t.piau@rubika-edu.com>
2019-11-19 18:18:23 +0100

    Refactoring projet + Base Phase Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance;
+    private PhaseManager phaseManager;
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(this);
+            return;
+        } 
+        
+        instance = this;
+        phaseManager = new PhaseManager();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void SetGameSettings()
+    {
+
+    }
+
+    private void StartGameLoop()
+    {
+        phaseManager.Initiate();
+    }
+}
---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Phase
+{
+    INIT,
+    DEFENCE,
+    ATTACK,
+    RESOLUTION
+}
+
+public class PhaseManager : MonoBehaviour
+{
+    private Phase currentPhase;
+    private Coroutine currentPhaseCoroutine;
+
+    private List<GameObject> attackers;
+    private List<GameObject> defenders;
+
+    public PhaseManager()
+    {
+        currentPhase = Phase.INIT;
+    }
+
+    public void Initiate()
+    {
+        currentPhaseCoroutine = StartCoroutine(InitPhase());
+    }
+
+    private IEnumerator InitPhase()
+    {
+        currentPhase = Phase.INIT;
+
+        // Afficher de l'UI (Debuter Round?)
+
+        bool isClicked = false;
+        while(!isClicked)
+        {
+            // Verifier si le bouton/ecran a été click 
+            yield return 0;
+        }
+        
+        currentPhaseCoroutine = StartCoroutine(DefencePhase());
+    }
+
+    private IEnumerator DefencePhase()
+    {
+        currentPhase = Phase.DEFENCE;
+
+        // Afficher de L'UI
+
+        defenders.Clear();
+
+        bool isValidate = false;
+        while(!isValidate)
+        {
+            // Check les cartes
+            // Ajouter les units correspondante à la liste Defenders
+            // + Ajouter les units à la scène
+            yield return 0;
+        }
+
+        // Desafficher L'UI
+
+        currentPhaseCoroutine = StartCoroutine(AttackPhase());
+    }
+
+    private IEnumerator AttackPhase()
+    {
+        currentPhase = Phase.ATTACK;
+
+        //Afficher de L'UI
+
+        attackers.Clear();
+
+        bool isValidate = false;
+        while (!isValidate)
+        {
+            // Check les cartes
+            // Ajouter les units correspondante à la liste Defenders
+            // + Ajouter les units à la scène
+            yield return 0;
+        }
+        
+        // Desafficher l'UI
+
+        currentPhaseCoroutine = StartCoroutine(ResolutionPhase());
+    }
+
+    private IEnumerator ResolutionPhase()
+    {
+        currentPhase = Phase.RESOLUTION;
+
+        // Afficher l'UI?
+
+        bool isResolved = false;
+        while(!isResolved)
+        {
+            // Action des attackers
+            // Action des defenders
+            yield return 0;
+        }
+
+        // Afficher le résultat
+
+        bool isClicked = false;
+        while(!isClicked)
+        {
+            // Attendre que le joueur veuille start le prochain round (click bouton/ecran)
+            yield return 0;
+        }
+
+        // Desafficher l'UI
+
+        currentPhaseCoroutine = StartCoroutine(DefencePhase());
+    }
+
+    public Phase GetCurrentPhase()
+    {
+        return currentPhase;
+    }
+}
