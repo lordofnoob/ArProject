7d37ac6440283eb3e8d5fbc19f087b440e08c0e2
PIAU Timothée <t.piau@rubika-edu.com>
2019-11-20 20:08:18 +0100

    TileManager Fonctionnel

---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+    OBSTACLE,
+    GROUND,
+    ATTACKSPAWN,
+    DEFENCESPAWN
+}
+
+public class TileInfo : MonoBehaviour
+{
+    public int distanceFromGoal;
+    public TileType tileType;
+}
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -23,7 +23,16 @@ public class TileManager : MonoBehaviour
     {
         tileCount = rowCount * (lineCount / 2) + (lineCount % 2) * (rowCount / 2 + rowCount % 2);
         tileGrid = new GameObject[tileCount];
-        originTransform = originTransformDebug;
+        
+        if(originTransformDebug)
+        {
+            originTransform = originTransformDebug;
+        }
+        else
+        {
+            originTransform.position = Vector3.zero;
+            originTransform.rotation = Quaternion.identity;
+        }
 
         InstanciateGrid();
     }
@@ -35,29 +44,13 @@ public class TileManager : MonoBehaviour
             tileGrid[i] = Instantiate(tilePrefab) as GameObject;
             tileGrid[i].transform.parent = this.transform;
             tileGrid[i].transform.localPosition = GetTilePosition(i);
-            tileGrid[i].transform.localRotation = Quaternion.identity;
+            tileGrid[i].transform.localRotation = originTransform.rotation;
         }
     }
 
-    public Vector3 GetTilePosition(int tileID)
+    public void SetDynamicPathFinding(int goalTileID)
     {
-        int tileLine = GetLine(tileID);
-        int tileRow = GetRow(tileID);
 
-        if (rowCount % 2 == 0)
-        {
-            if (lineCount % 2 == 0)
-                return new Vector3((tileRow - rowCount / 2 + 1f/2f) * Mathf.Sqrt(3) * tileSize / 4f, originTransform.position.y, (lineCount / 2 - tileLine + 1f/2f) * tileSize / 2f);
-            else
-                return new Vector3((tileRow - rowCount / 2 + 1f/2f) * Mathf.Sqrt(3) * tileSize / 4f , originTransform.position.y, (lineCount / 2 - tileLine) * tileSize / 2f);
-        }
-        else
-        {
-            if (lineCount % 2 == 0)
-                return new Vector3((tileRow - rowCount / 2 + 1) * Mathf.Sqrt(3) * tileSize / 2f, originTransform.position.y, (lineCount / 2 - tileLine + 1f/2f) * tileSize / 2f);
-            else
-                return new Vector3((tileRow - rowCount / 2 + 1) * Mathf.Sqrt(3) * tileSize / 2f, originTransform.position.y, (lineCount / 2 - tileLine) * tileSize / 2f);
-        }
     }
 
     public GameObject GetTile(int tileID)
@@ -67,7 +60,12 @@ public class TileManager : MonoBehaviour
 
     public GameObject GetTile(int row, int line)
     {
-        return tileGrid[(line/2) * (rowCount) + ((row+1)%2) * (row/2 + row%2) + (row%2) * (rowCount/2 + rowCount%2 + row/2)];
+        return tileGrid[GetTileID(row, line)];
+    }
+
+    public int GetTileID(int row, int  line)
+    {
+        return (line / 2) * (rowCount) + ((row + 1) % 2) * (row / 2) + (row % 2) * ((rowCount / 2) + (rowCount % 2) - 1 + ((row + 1) / 2));
     }
 
     //public GameObject GetTile(int x, int y)
@@ -75,60 +73,151 @@ public class TileManager : MonoBehaviour
     //    return;
     //}
 
+    public Vector3 GetTilePosition(int tileID)
+    {
+        int tileLine = GetLine(tileID);
+        int tileRow = GetRow(tileID);
+
+        if (rowCount % 2 == 0)
+        {
+            if (lineCount % 2 == 0)
+                return new Vector3((tileRow - rowCount / 2 + 1f/2f) * Mathf.Sqrt(3) * tileSize, originTransform.position.y, (lineCount / 2 - tileLine - 1f/2f) * tileSize);
+            else
+                return new Vector3((tileRow - rowCount / 2 + 1f/2f) * Mathf.Sqrt(3) * tileSize, originTransform.position.y, (lineCount / 2 - tileLine) * tileSize);
+        }
+        else
+        {
+            if (lineCount % 2 == 0)
+                return new Vector3((tileRow - rowCount/2) * Mathf.Sqrt(3) * tileSize, originTransform.position.y, (lineCount / 2 - tileLine - 1f/2f) * tileSize); //(tileRow - rowCount / 2 + 1) * Mathf.Sqrt(3) * tileSize / 2f
+            else
+                return new Vector3((tileRow - rowCount/2) * Mathf.Sqrt(3) * tileSize, originTransform.position.y, (lineCount / 2 - tileLine) * tileSize);
+        }
+    }
+
+
     private int GetLine(int tileID)
     {
-        if (tileID % rowCount > (rowCount / 2 + rowCount % 2))
-            return 2 * tileID / rowCount + 1;
+        if (tileID % rowCount > (rowCount / 2 + rowCount % 2 - 1))
+            return 2 * (tileID / rowCount) + 1;  
         else
-            return 2 * tileID / rowCount;
+            return 2 * ((tileID + 1) / rowCount);
     }
 
     private int GetRow(int tileID)
     {
-        return tileID % rowCount;
+        if (tileID % rowCount > ((rowCount / 2) + (rowCount % 2) - 1))
+            return 2 * (tileID % rowCount - (rowCount / 2 + rowCount % 2)) + 1;
+        else
+            return 2 * (tileID % rowCount);
     }
 
-    public List<GameObject> GetTileNeighbours(int tileID)
+    public List<int> GetTileNeighbours(int tileID)
     {
-        List<GameObject> neighbours = new List<GameObject>();
+        List<int> neighbours = new List<int>();
 
         int tileLine = GetLine(tileID);
         int tileRow = GetRow(tileID);
 
         if (tileLine > 1)
         {
-            neighbours.Add(GetTile(tileRow, tileLine - 1));
+            neighbours.Add(GetTileID(tileRow, tileLine - 1));
         }
         
         if(tileRow < rowCount - 1 )
         {
             if (tileLine > 0)
             {
-                neighbours.Add(GetTile(tileRow + 1, tileLine - 1));
+                neighbours.Add(GetTileID(tileRow + 1, tileLine - 1));
             }
             if (tileLine < lineCount - 1)
             {
-                neighbours.Add(GetTile(tileRow + 1, tileLine + 1));
+                neighbours.Add(GetTileID(tileRow + 1, tileLine + 1));
             }
         }
 
         if (tileLine < lineCount - 2)
         {
-            neighbours.Add(GetTile(tileRow, tileLine + 2));
+            neighbours.Add(GetTileID(tileRow, tileLine + 2));
         }
 
         if(tileRow > 0)
         {
             if(tileLine > 0)
             {
-                neighbours.Add(GetTile(tileRow - 1, tileLine - 1));
+                neighbours.Add(GetTileID(tileRow - 1, tileLine - 1));
             }
             if(tileLine < lineCount - 1)
             {
-                neighbours.Add(GetTile(tileRow - 1, tileLine + 1));
+                neighbours.Add(GetTileID(tileRow - 1, tileLine + 1));
             }
         }
 
         return neighbours;
+    }
+
+    // A Tester
+    public List<List<GameObject>> GetTileInRange(int centerTileID, int range)
+    {
+        List<List<int>> tilesInRange = new List<List<int>>();
+        List<int> discoveredTilesID = new List<int>();
+
+        discoveredTilesID.Add(centerTileID);
+
+        tilesInRange.Add(GetTileNeighbours(centerTileID));
+        discoveredTilesID.AddRange(GetTileNeighbours(centerTileID)); // A mettre dans une variable?
+
+        for(int i = 1; i < range; i++)
+        {
+            List<int> tilesInRangeI = new List<int>();
+            
+            foreach(int tileID in tilesInRange[tilesInRange.Count-1])
+            {
+                List<int> currentTileNeighbours = GetTileNeighbours(tileID);
+
+                //Remove duplicate
+                for(i = currentTileNeighbours.Count - 1; i >= 0; i--)
+                {
+                    foreach(int discoveredTile in discoveredTilesID)
+                    {
+                        if(discoveredTile == currentTileNeighbours[i])
+                        {
+                            currentTileNeighbours.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+
+                tilesInRangeI.AddRange(currentTileNeighbours);
+                discoveredTilesID.AddRange(currentTileNeighbours);
+            }
+
+            tilesInRange.Add(tilesInRangeI);
+        }
+
+        return CastRangeList(tilesInRange);
+    }
+
+    private List<List<GameObject>> CastRangeList(List<List<int>> tileListIDs)
+    {
+        List<List<GameObject>> tileList = new List<List<GameObject>>();
+
+        for(int i = 0; i < tileListIDs.Count; i++)
+        {
+            tileList.Add(IDListToGameObjectList(tileListIDs[i]));
+        }
+
+        return tileList;
+    }
+
+    private List<GameObject> IDListToGameObjectList(List<int> iDList)
+    {
+        List<GameObject> gameObjectList = new List<GameObject>();
+
+        foreach(int iD in iDList)
+        {
+            gameObjectList.Add(GetTile(iD));
+        }
+
+        return gameObjectList;
     }
 }
