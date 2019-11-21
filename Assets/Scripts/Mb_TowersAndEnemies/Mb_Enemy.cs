5b497e1e9364218055316ffae3c246fd54f39cfd
PIAU Timothée <t.piau@rubika-edu.com>
2019-11-21 20:17:03 +0100

    Version Test path finding - Bug

--- Assets/Scripts/Mb_TowersAndEnemies/Mb_Enemy.cs
+++ Assets/Scripts/Mb_TowersAndEnemies/Mb_Enemy.cs
@@ -7,13 +7,18 @@ public enum UnitState
     IDLE,
     STANDBY,
     MOVING,
+    ATTACKING,
     DEAD
 }
 
 public class Mb_Enemy : MonoBehaviour
 {
+    [Header("Initialisation")]
+    public int spawnRow;
+    public int spawnLine;
+
     [Header("Characteritics")]
-    public Sc_Monsters monsterCharacteristics;
+    //public Sc_Monsters monsterCharacteristics;
     public allCharacterisitcs monsterUpdatedCharacteristics;
 
     [Header("GraphicPart")]
@@ -26,8 +31,20 @@ public class Mb_Enemy : MonoBehaviour
 
     private void Awake()
     {
-        monsterUpdatedCharacteristics = monsterCharacteristics.monsterBaseCharacteristics;
+        //monsterUpdatedCharacteristics = monsterCharacteristics.monsterBaseCharacteristics;
         unitDestinationTile = -1;
+        unitState = UnitState.STANDBY;
+    }
+
+    public void InitializePosition()
+    {
+        gameObject.transform.parent = TileManager.instance.transform;
+        gameObject.transform.localPosition = TileManager.instance.GetTilePosition(TileManager.instance.GetTileID(spawnRow, spawnLine));
+    }
+
+    public UnitState GetUnitState()
+    {
+        return unitState;
     }
 
     public int GetFromTile()
@@ -54,29 +71,42 @@ public class Mb_Enemy : MonoBehaviour
         if (unitState == UnitState.STANDBY)
         {
             unitDestinationTile = TileManager.instance.GetTileInfo(unitStartingMovementTile).FindNextTile();
-            unitState = UnitState.MOVING;
-            movementProgress = 0;
-            transform.localRotation = Quaternion.LookRotation(TileManager.instance.GetTilePosition(unitDestinationTile) - TileManager.instance.GetTilePosition(unitStartingMovementTile), TileManager.instance.transform.up);
+            Debug.Log(unitDestinationTile);
+            Debug.Log(TileManager.instance.GetTileInfo(unitDestinationTile).distanceFromGoal);
+            if(unitDestinationTile == 0)
+            {
+                unitState = UnitState.ATTACKING;
+            }
+            else
+            {
+                unitState = UnitState.MOVING;
+                movementProgress = 0;
+                transform.localRotation = Quaternion.LookRotation(TileManager.instance.GetTilePosition(unitDestinationTile) - TileManager.instance.GetTilePosition(unitStartingMovementTile), TileManager.instance.transform.up);
+            }
         }
         if(unitState == UnitState.MOVING)
         {
             Move();
         }
+        if(unitState == UnitState.ATTACKING)
+        {
+            Debug.Log("Attacking!");
+        }
     }
 
     private void Move()
     {
-        movementProgress += (int)monsterUpdatedCharacteristics.speed / 100 * Time.fixedDeltaTime;
+        movementProgress += (float)monsterUpdatedCharacteristics.speed / 100f * Time.fixedDeltaTime;
 
         if(movementProgress >= 1)
         {
-            transform.position = TileManager.instance.GetTilePosition(unitDestinationTile);
+            transform.localPosition = TileManager.instance.GetTilePosition(unitDestinationTile);
             unitStartingMovementTile = unitDestinationTile;
             unitState = UnitState.STANDBY;
             return;
         }
 
-        transform.position = TileManager.instance.GetTilePosition(unitStartingMovementTile) + (TileManager.instance.GetTilePosition(unitDestinationTile) - TileManager.instance.GetTilePosition(unitStartingMovementTile)) / movementProgress;
+        transform.localPosition = TileManager.instance.GetTilePosition(unitStartingMovementTile) + ( TileManager.instance.GetTilePosition(unitDestinationTile) - TileManager.instance.GetTilePosition(unitStartingMovementTile) ) * movementProgress;
     }
 
     
---
+++ Assets/Scripts/TestGameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGameManager : MonoBehaviour
+{
+    public static TestGameManager instance;
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public int nexusTileID;
+    public List<Mb_Enemy> attackers;
+    public Coroutine currentCoroutine;
+    public System.Random random;
+    
+
+    private void Start()
+    {
+        random = new System.Random();
+        currentCoroutine = StartCoroutine(Initialization());
+    }
+
+    private IEnumerator Initialization()
+    {
+        yield return new WaitForFixedUpdate();
+        TileManager.instance.InstanciateGrid();
+        Debug.Log("Grid Instanciate");
+        TileManager.instance.SetPathFinding(nexusTileID);
+        Debug.Log("PathFinding Setup Done!");
+
+        for (int i = 0; i < attackers.Count; i++)
+        {
+            attackers[i].InitializePosition();
+        }
+
+        currentCoroutine = StartCoroutine(Resolution());
+    }
+
+    private IEnumerator Resolution()
+    {
+        while(true)
+        {
+            foreach(Mb_Enemy attacker in attackers)
+            {
+                attacker.Action();
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
