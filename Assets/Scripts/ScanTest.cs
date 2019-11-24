99996b4dde1daf1465dc863bd8fe507db299b6d0
PIAU Timothée <t.piau@rubika-edu.com>
2019-11-24 15:05:54 +0100

    Scan Test

---
+++ Assets/Scripts/ScanTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using Image = UnityEngine.UI.Image;
+public class ScanTest : MonoBehaviour
+{
+    public ImageTargetBehaviour TopLeftCornerImageTarget, DownRightCornerImageTarget;
+    private GameObject topPos, botPos;
+    public GameObject topPoseObject, botPoseObject;
+    public Text debugText, debuggerDebugText;
+
+    private void Update()
+    {
+        if (TopLeftCornerImageTarget.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            debuggerDebugText.text = TopLeftCornerImageTarget.transform.position.ToString();
+        }
+        if (DownRightCornerImageTarget.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            debugText.text = DownRightCornerImageTarget.transform.position.ToString();
+        }
+    }
+
+    public void Scan()
+    {
+        if (TopLeftCornerImageTarget.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            debuggerDebugText.text = TopLeftCornerImageTarget.transform.position.ToString();
+            botPos = Instantiate(topPoseObject, TopLeftCornerImageTarget.transform.position, Quaternion.identity);
+
+        }
+        if (DownRightCornerImageTarget.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            debugText.text = DownRightCornerImageTarget.transform.position.ToString();
+            topPos = Instantiate(botPoseObject, DownRightCornerImageTarget.transform.position, Quaternion.identity);
+        }
+
+        if (topPos != null && botPos != null)
+            debugText.text = "recognised";
+          //  TileManager.instance.SetTileGridTransform((topPos.transform.position - botPos.transform.position) / 2 + botPos.transform.position);
+
+        Debug.Log("SCAN");
+    }
+}
